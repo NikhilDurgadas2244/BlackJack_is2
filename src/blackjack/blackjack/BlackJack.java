b2c57f958223e52d891c32967ff2c50955f87d5a
NikhilDurgadas <Nikhil@DESKTOP-M2DV3PQ>
2021-12-30 12:25:43 +0000

    BlackJack - versión 5

--- src/blackjack/blackjack/BlackJack.java
+++ src/blackjack/blackjack/BlackJack.java
@@ -11,12 +11,14 @@ public class BlackJack {
     private final Player player2;
     private final Player player3;
     private final Player croupier;
+    private final Player deck;
 
-    public BlackJack(Player p1, Player p2, Player p3, Player c) {
+    public BlackJack(Player p1, Player p2, Player p3, Player c, Player d) {
         this.player1 = p1;
         this.player2 = p2;
         this.player3 = p3;
         this.croupier = c;
+        this.deck = d;
     }
     
     public static boolean isBlackJack(List<Card> bet){
@@ -39,25 +41,89 @@ public class BlackJack {
         return totalValue;
     }
     
+    public static List<Card> croupierTakeCards(List<Card> croupierCards, List<Card> deckCards){
+        
+         while (calculatePoints(croupierCards) < 17){
+             for (Card card : deckCards){
+                 croupierCards.add(card);
+            }
+         }
+         return croupierCards;
+    }
+    
     public List<Player> getWinners(){
-        
         ArrayList<Player> winners = new ArrayList<>();
+        List<Card> p1Cards = new ArrayList<>(player1.getCards());
+        List<Card> p2Cards = new ArrayList<>(player2.getCards());
+        List<Card> p3Cards = new ArrayList<>(player3.getCards());
+        List<Card> croupierCards = new ArrayList<>(croupierTakeCards(croupier.getCards(), deck.getCards()));
         
+        int p1Points = calculatePoints(p1Cards);
+        int p2Points = calculatePoints(p2Cards);
+        int p3Points = calculatePoints(p3Cards);
+        int croupierPoints = calculatePoints(croupierCards);
+      
+        if(croupierPoints > 21){
+            winners.add(player1);
+            winners.add(player2);
+            winners.add(player3);
+            return winners;
+        }
+        
+        if(isBlackJack(croupierCards)){
+            winners.add(croupier);
+            return winners;
+        }
+       
+        if(croupierPoints > p1Points && croupierPoints > p2Points && croupierPoints > p3Points && croupierPoints < 21){
+            winners.add(croupier);
+            return winners;
+        }
+        
+        if(isBlackJack(p1Cards)){
+            winners.add(player1);
+        }
+        
+        if(isBlackJack(p2Cards)){
+            winners.add(player2);
+        }
+        
+        if(isBlackJack(p3Cards)){
+            winners.add(player3);
+        }
+        
+        /*
+        else if(isBlackJack(p1Cards) || (calculatePoints(player1.getCards()) > calculatePoints(croupierCards))){
+            winners.add(player1);
+        }
+        
+        if(winners.isEmpty()){
+            winners.add(croupier);
+        }
+        */
+        
+
+        /*
         if(isBlackJack(player1.getCards())){
+            winners.add(player1);
+        }
         
+        else if(calculatePoints(player1.getCards()) > calculatePoints(croupierTakeCards(croupier.getCards(), deck.getCards()))){
             winners.add(player1);
         }
         
         if(isBlackJack(player2.getCards())){
-        
             winners.add(player2);
         }
         
         if(isBlackJack(player3.getCards())){
-        
             winners.add(player3);
         }
         
+        if(isBlackJack(croupierTakeCards(croupier.getCards(), deck.getCards()))){
+            winners.add(croupier);
+        }
+        */
         return winners;
     }
     
--- test/test/test_blackjack.java
+++ test/test/test_blackjack.java
@@ -43,13 +43,45 @@ public class test_blackjack {
         deck.addCard(new King());
         deck.addCard(new PipCard(2));
         
-        BlackJack blackjack = new BlackJack(player1,player2,player3,croupier);
+        BlackJack blackjack = new BlackJack(player1,player2,player3,croupier,deck);
         List<Player> winners = blackjack.getWinners();
         List<Player> test = new ArrayList<>();
         test.add(player1);
         assert(winners.equals(test));
     }
     
+    
+    @Test
+    public void prueba_caso_2(){
+        player1.addCard(new PipCard(10));
+        player1.addCard(new Ace());
+        
+        player2.addCard(new PipCard(10));
+        player2.addCard(new PipCard(2));
+        player2.addCard(new PipCard(6));
+        
+        player3.addCard(new PipCard(10));
+        player3.addCard(new Ace());
+        
+        croupier.addCard(new PipCard(5));
+        croupier.addCard(new PipCard(10));
+        
+        deck.addCard(new Ace());
+        deck.addCard(new PipCard(3));
+        deck.addCard(new King());
+        deck.addCard(new PipCard(2));
+        
+         BlackJack blackjack = new BlackJack(player1,player2,player3,croupier, deck);
+        List<Player> winners = blackjack.getWinners();
+        List<Player> test = new ArrayList<>();
+        test.add(player1);
+        test.add(player3);
+        assert(winners.equals(test));
+        
+        
+        
+    
+    }
     
     
     
